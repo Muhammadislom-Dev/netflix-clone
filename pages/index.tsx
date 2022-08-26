2920190a32933372cdc4b7b195c9a86defa93cba
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-27 02:34:32 +0500

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,29 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Header from '../components/Header'
+
+
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Netflix Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>Hello world</div>
+      <Header />
+      <main>
+          {/* Baner  */}
+          <section>
+              {/* Row  */}
+              {/* Row  */}
+              {/* Row  */}
+              {/* Row  */}
+              {/* Row  */}
+          </section>
+          {/* Modal  */}
+      </main>
     </div>
   )
 }
