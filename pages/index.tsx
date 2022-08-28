9874dd9828c85978fd174515c6a717c4ba88d645
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-28 16:03:00 +0500

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Barner from '../components/Barner'
 import Header from '../components/Header'
+import requests from '../utils/requests'
 
 
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#0105111] lg:h-[140vh]'>
       <Head>
         <title>Netflix Clone</title>
         <link rel="icon" href="/favicon.ico" />
@@ -14,7 +16,7 @@ const Home: NextPage = () => {
 
       <Header />
       <main>
-          {/* Baner  */}
+          <Barner />
           <section>
               {/* Row  */}
               {/* Row  */}
@@ -29,3 +31,27 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+
+export const getServerSideProps = async () => {
+  
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+}
