4089de20beb2850d2ce809dc60cc13fce8cca6cf
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-28 16:02:33 +0500

    add

---
+++ components/Barner.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+function Barner() {
+  return (
+    <div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Barner
