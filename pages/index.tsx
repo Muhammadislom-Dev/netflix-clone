7fe49559c2dab0aae9bfebb8ce3708247e563d3a
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-28 16:24:22 +0500

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,35 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Barner from '../components/Barner'
 import Header from '../components/Header'
+import { Movie } from '../typings,d'
 import requests from '../utils/requests'
 
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({  
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
 
 
-const Home: NextPage = () => {
+  console.log(netflixOriginals);
+  
+
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#0105111] lg:h-[140vh]'>
       <Head>
@@ -34,7 +58,7 @@ export default Home
 
 
 export const getServerSideProps = async () => {
-  
+
   const [
     netflixOriginals,
     trendingNow,
@@ -54,4 +78,17 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
     fetch(requests.fetchDocumentaries).then((res) => res.json()),
   ])
+
+  return{
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    }
+  }
 }
