78f44c777c70689d5e031765467a3ed5f0394c5e
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-30 02:46:30 +0500

    add

---
+++ components/Row.tsx
@@ -0,0 +1,24 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import React from 'react'
+import { Movie } from '../typings'
+
+interface Props {
+    title: string
+    movies: Movie[] 
+  }
+
+function Row({ title, movies }: Props) {
+  return (
+    <div className='h-40 space-y-0.5 md:space-y-2'>
+        <h1>{title}</h1>
+        <div className='group relative md:ml-2'>
+           <ChevronLeftIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
+            cursor-pointer opacity-0 transition hover:scale-175 group-hover:opacity-100' />
+           <ChevronRightIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
+            cursor-pointer opacity-0 transition hover:scale-175 group-hover:opacity-100'  />
+        </div>
+    </div>
+  )
+}
+
+export default Row
