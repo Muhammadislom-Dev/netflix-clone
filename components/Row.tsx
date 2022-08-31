45d2b37373a720354d4288d888e57d020d051e48
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-31 14:04:11 +0500

    add

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,6 +1,7 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
 import React from 'react'
 import { Movie } from '../typings'
+import Thumnail from './Thumnail'
 
 interface Props {
     title: string
@@ -14,6 +15,13 @@ function Row({ title, movies }: Props) {
         <div className='group relative md:ml-2'>
            <ChevronLeftIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
             cursor-pointer opacity-0 transition hover:scale-175 group-hover:opacity-100' />
+
+          <div className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2'>
+             <Thumnail />
+             <Thumnail />
+             <Thumnail />
+          </div>
+            
            <ChevronRightIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
             cursor-pointer opacity-0 transition hover:scale-175 group-hover:opacity-100'  />
         </div>
