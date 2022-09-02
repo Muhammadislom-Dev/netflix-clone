c0a20ae72a8278fc7c25b5934b5beab7c6012312
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-03 03:07:21 +0500

    add

--- components/Thumnail.tsx
+++ components/Thumnail.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { Movie } from "../typings";
 
-function Thumnail() {
-  return (
-    <div>Thumnail</div>
-  )
+interface Props {
+  movie: Movie;
 }
 
-export default Thumnail
+function Thumnail({ movie }: Props) {
+  return (
+    <div className="relative h-28 min-w-[180px] transition duration-200 ease-out
+    md:h-36 md:min-w-[260px] md:hover:scale-105 cursor-pointer ">
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"
+      />
+    </div>
+  );
+}
+
+export default Thumnail;
