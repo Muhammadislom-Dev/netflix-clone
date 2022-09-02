9e67b8bc3aa9bc2f7459fa28df2d8c5b4d4c1f20
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-03 03:07:29 +0500

    add

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Movie } from '../typings'
 import Thumnail from './Thumnail'
 
@@ -9,21 +9,44 @@ interface Props {
   }
 
 function Row({ title, movies }: Props) {
+
+  const rowRef = useRef<HTMLDivElement>(null)
+  const [isMoved, setIsMoved] = useState(false)
+
+  const handleClick = (direction: string) => {
+    setIsMoved(true)
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current
+
+      const scrollTo =
+        direction === 'left'
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth
+      rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' })
+    }
+  }
+
   return (
     <div className='h-40 space-y-0.5 md:space-y-2'>
         <h1>{title}</h1>
         <div className='group relative md:ml-2'>
-           <ChevronLeftIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
-            cursor-pointer opacity-0 transition hover:scale-175 group-hover:opacity-100' />
+           <ChevronLeftIcon className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
+            cursor-pointer opacity-0 transition hover:scale-175 group-hover:opacity-100 ${isMoved && "hidden" } `}
+            onClick={() => handleClick("left")}
+            />
 
-          <div className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2'>
-             <Thumnail />
-             <Thumnail />
-             <Thumnail />
+          <div ref={rowRef} className='flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll  md:space-x-2.5 md:p-2'>
+            {
+              movies.map((movie) => (
+                <Thumnail key={movie.id} movie={movie} />
+              ))
+            }
           </div>
             
-           <ChevronRightIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
-            cursor-pointer opacity-0 transition hover:scale-175 group-hover:opacity-100'  />
+           <ChevronRightIcon className='absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9
+            cursor-pointer opacity-0 transition hover:scale-175 group-hover:opacity-100' 
+            onClick={() => handleClick("left")}
+            />
         </div>
     </div>
   )
