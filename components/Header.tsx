f06b0fccb596086b8659b0a0b4ad1705816738e8
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-04 17:56:07 +0500

    add

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,12 @@
 import { BellIcon, SearchIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import useAuth from '../hooks/useAuth'
 
 function Header() {
 
     const [isScrolled, setIsScrolled] = useState(false)
+    const {logout} = useAuth()
 
     useEffect(() => {
         const handleScroll = () => {
@@ -43,13 +45,14 @@ function Header() {
             <SearchIcon className=" hidden h-6 w-6 sm:inline" />
             <p className='hidden lg:inline'>Kids</p>
             <BellIcon  className='h-6 w-8 ' />
-            <Link href="/account" >
+            {/* <Link href="/account" > */}
                 <img
+                onClick={logout}
                 src="https://rb.gy/g1pwyx"
                 alt=""
                 className="cursor-pointer rounded"
                 />
-            </Link>
+            {/* </Link> */}
         </div>
     </header>
   )
