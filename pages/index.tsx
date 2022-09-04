158c20186bbfd2564b6fa992adf03abeb13dbb84
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-04 17:56:19 +0500

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,26 @@
-import Head from 'next/head'
-import Barner from '../components/Barner'
-import Header from '../components/Header'
-import Row from '../components/Row'
-import { Movie } from '../typings.d'
-import requests from '../utils/requests'
+import Head from "next/head";
+import { useRecoilValue } from "recoil";
+import { modalState } from "../atoms/modalAtom";
+import Barner from "../components/Barner";
+import Header from "../components/Header";
+import Modal from "../components/Modal";
+import Row from "../components/Row";
+import useAuth from "../hooks/useAuth";
+import { Movie } from "../typings.d";
+import requests from "../utils/requests";
 
 interface Props {
-  netflixOriginals: Movie[]
-  trendingNow: Movie[]
-  topRated: Movie[]
-  actionMovies: Movie[]
-  comedyMovies: Movie[]
-  horrorMovies: Movie[]
-  romanceMovies: Movie[]
-  documentaries: Movie[]
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
 }
 
-const Home = ({  
+const Home = ({
   netflixOriginals,
   actionMovies,
   comedyMovies,
@@ -26,22 +30,22 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
+  const { loading } = useAuth()
+  const showModal = useRecoilValue(modalState);
 
-
-  console.log(netflixOriginals);
-  
+  if (loading) return null;
 
   return (
-    <div className='relative h-screen bg-gradient-to-b  lg:h-[140vh]'>
+    <div className="relative h-screen bg-gradient-to-b  lg:h-[140vh]">
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      <main className='relative pl-4 pb-24 lg:space-y-4 lg:pl-16'>
-          <Barner netflixOriginals={netflixOriginals} />
-          <section className="md:space-y-24">
+      <main className="relative pl-4 pb-24 lg:space-y-4 lg:pl-16">
+        <Barner netflixOriginals={netflixOriginals} />
+        <section className="md:space-y-24">
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
@@ -50,18 +54,16 @@ const Home = ({
           <Row title="Scary Movies" movies={horrorMovies} />
           <Row title="Romance Movies" movies={romanceMovies} />
           <Row title="Documentaries" movies={documentaries} />
-          </section>
-          {/* Modal  */}
+        </section>
       </main>
+        {showModal && <Modal />}
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
 
 export const getServerSideProps = async () => {
-
   const [
     netflixOriginals,
     trendingNow,
@@ -80,9 +82,9 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchHorrorMovies).then((res) => res.json()),
     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
     fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ])
+  ]);
 
-  return{
+  return {
     props: {
       netflixOriginals: netflixOriginals.results,
       trendingNow: trendingNow.results,
@@ -92,6 +94,6 @@ export const getServerSideProps = async () => {
       horrorMovies: horrorMovies.results,
       romanceMovies: romanceMovies.results,
       documentaries: documentaries.results,
-    }
-  }
-}
+    },
+  };
+};
