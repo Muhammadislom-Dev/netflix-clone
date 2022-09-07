4ef43bae732247fc1dee838f877757004947b309
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-08 01:09:29 +0500

    commit

--- components/Thumnail.tsx
+++ components/Thumnail.tsx
@@ -1,15 +1,25 @@
-import Image from "next/image";
-import React from "react";
-import { Movie } from "../typings";
+import { DocumentData } from 'firebase/firestore'
+import Image from 'next/image'
+import { useRecoilState } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom.'
+import { Movie } from '../typings'
 
 interface Props {
-  movie: Movie;
+  movie: Movie | DocumentData
 }
 
-function Thumnail({ movie }: Props) {
+function Thumbnail({ movie }: Props) {
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  const [showModal, setShowModal] = useRecoilState(modalState)
+
   return (
-    <div className="relative h-28 min-w-[180px] transition duration-200 ease-out
-    md:h-36 md:min-w-[260px] md:hover:scale-105 cursor-pointer ">
+    <div
+      className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
+      onClick={() => {
+        setCurrentMovie(movie)
+        setShowModal(true)
+      }}
+    >
       <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
@@ -18,7 +28,7 @@ function Thumnail({ movie }: Props) {
         layout="fill"
       />
     </div>
-  );
+  )
 }
 
-export default Thumnail;
+export default Thumbnail
