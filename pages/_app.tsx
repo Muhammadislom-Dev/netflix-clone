1c322046dde24d9c21f3b11f6821641a88d5ad9d
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-08 01:10:11 +0500

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { AuthProvider } from "../hooks/useAuth";
+import type { AppProps } from 'next/app'
+import { AuthProvider } from '../hooks/useAuth'
+import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
+    <RecoilRoot>
+      {/* Higher Order Component */}
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </RecoilRoot>
+  )
 }
 
-export default MyApp;
+export default MyApp
